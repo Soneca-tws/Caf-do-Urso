980c22cc8c59de2e47095cdb3895963eb3f5e3d9
Otávio <pedro.otavio.cg@hotmail.com>
2026-09-09 12:10:30 -0300

    Base de sabotagem de Maquina

--- Assets/Scripts/ShotgunShoot.cs
+++ Assets/Scripts/ShotgunShoot.cs
@@ -56,11 +56,15 @@ public class ShotgunShoot : MonoBehaviour
 
                 // Tenta buscar o script EnemyAI no objeto atingido (ou nos pais caso o colisor esteja numa sub-parte)
                 EnemyAI enemy = hit.collider.GetComponentInParent<EnemyAI>();
+                MachineSabotage machine = hit.collider.GetComponentInParent<MachineSabotage>();
 
-                // Se encontrou o script do inimigo, aplica o dano correspondente ao perdigão da escopeta
                 if (enemy != null)
                 {
                     enemy.TakeDamage((int)damagePerPellet);
+                }
+                else if (machine != null)
+                {
+                    machine.TakeDamage((int)damagePerPellet);
                 }
             }
         }
